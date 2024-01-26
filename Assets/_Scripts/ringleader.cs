78cb7aad1ac21d087258d8c2cb59a381c0ef3d07
Hissal <hissalofficial@gmail.com>
2024-01-26 21:22:16 +0200

    PlayerMovement
    left right jump

--- Assets/_Scripts/ringleader.cs
+++ Assets/_Scripts/ringleader.cs
@@ -1,48 +1,48 @@
-using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
-using System;
+//using System.Collections;
+//using System.Collections.Generic;
+//using UnityEngine;
+//using System;
 
-public class ringleader : MonoBehaviour
-{
-    // Start is called before the first frame update
-    void Start()
-    {
-        EventManager eventManager = new EventManager();
-    }
+//public class ringleader : MonoBehaviour
+//{
+//    // Start is called before the first frame update
+//    void Start()
+//    {
+//        EventManager eventManager = new EventManager();
+//    }
 
-    // Update is called once per frame
-    void Update()
-    {
+//    // Update is called once per frame
+//    void Update()
+//    {
         
-    }
-}
+//    }
+//}
 
-public class EventManager
-{
-    private List<(string)> events = new List<(string)>
-    {
-        ("LowGravity")
-    };
+//public class EventManager
+//{
+//    private List<string> events = new List<string>
+//    {
+//        ("LowGravity")
+//    };
 
-    private Dictionary<string, List<object>> eventParameters = new Dictionary<string, List<object>>();
-    {
-        {"LowGravity", new List<object> {"parameters"} }
-    }
+//    private Dictionary<string, List<object>> eventParameters = new Dictionary<string, List<object>>();
+//    {
+//        {"LowGravity", new List<object> {"parameters"} }
+//    }
 
-    private static void LowGravity(param)
-    {
-        // Do something
-    }
-    public void ExecuteEvent(string eventName)
-    {
-        var (name, action) = events.Find(x => x.Item1 == eventName);
-        action(eventParameters[eventName]);
-    }
+//    private static void LowGravity(param)
+//    {
+//        // Do something
+//    }
+//    public void ExecuteEvent(string eventName)
+//    {
+//        var (name, action) = events.Find(x => x.Item1 == eventName);
+//        action(eventParameters[eventName]);
+//    }
 
-    public void RandomEvent()
-    {
-        var randomEvent = events[UnityEngine.Random.Range(0, events.Count)];
-        randomEvent.Item2(eventParameters[randomEvent.Item1]);
-    }
-}
+//    public void RandomEvent()
+//    {
+//        var randomEvent = events[UnityEngine.Random.Range(0, events.Count)];
+//        randomEvent.Item2(eventParameters[randomEvent.Item1]);
+//    }
+//}
