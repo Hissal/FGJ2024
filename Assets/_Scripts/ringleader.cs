ddd907876dcfcd648b688dbdf6831ba4f2249d0e
Tuomas Niemi <tuomas.v.niemi@gmail.com>
2024-01-26 20:50:52 +0200

    Init ringmanager events

---
+++ Assets/_Scripts/ringleader.cs
@@ -0,0 +1,48 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System;
+
+public class ringleader : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        EventManager eventManager = new EventManager();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
+
+public class EventManager
+{
+    private List<(string, Action<string>)> events = new List<(string, Action<string>)>
+    {
+        ("LowGravity", LowGravity),
+    };
+
+    private Dictionary<string, List<object>> eventParameters = new Dictionary<string, List<object>>();
+    {
+        {"LowGravity", new List<object> {"parameters"} }
+    }
+
+    private static void LowGravity(param)
+    {
+        // Do something
+    }
+    public void ExecuteEvent(string eventName)
+    {
+        var (name, action) = events.Find(x => x.Item1 == eventName);
+        action(eventParameters[eventName]);
+    }
+
+    public void RandomEvent()
+    {
+        var randomEvent = events[UnityEngine.Random.Range(0, events.Count)];
+        randomEvent.Item2(eventParameters[randomEvent.Item1]);
+    }
+}
